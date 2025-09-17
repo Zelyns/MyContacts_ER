e059cd697abd2b7b2899c7f8066be539abb755e7
Etienne Rousseau <113974470+Zelyns@users.noreply.github.com>
2025-09-17 16:14:20 +0200

    Update index.jsx

--- client/src/components/signup/index.jsx
+++ client/src/components/signup/index.jsx
@@ -21,7 +21,7 @@ const Signup = () => {
     const handleSubmit = async(e) => {
         e.preventDefault();
         try {
-            const url = "http://localhost:8080/api/users";
+            const url = "https://mycontacts-er-backend.onrender.com";
             const {data: res} = await axios.post(url, data);
             navigate("/login");
             console.log(res.message);
